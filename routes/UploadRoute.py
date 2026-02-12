a0ad112291207cf518449db8c10a84a1062b0d0d
Chamitha <chamithat@codegen.net>
2026-02-12 14:44:23 +0530

    Vector DB setup and storing embeddings

--- routes/UploadRoute.py
+++ routes/UploadRoute.py
@@ -4,16 +4,17 @@ from services.FileService import FileService
 from services.PdfService import PDFService
 from services.ChunkService import ChunkService
 from services.EmbeddingSerivce import EmbeddingService
+from services.VectorStoreService import VectorStoreService
 from dotenv import load_dotenv
 
 load_dotenv()
-
 
 router = APIRouter(prefix="", tags=["Upload"])
 
 api_key = os.getenv("GOOGLE_API_KEY")
 if not api_key:
     raise ValueError("Missing GOOGLE_API_KEY environment variable")
+
 
 def get_file_service():
     return FileService()
@@ -31,17 +32,27 @@ def get_embeddings_service():
     return EmbeddingService(api_key=api_key)
 
 
+def get_vector_store_service():
+    return VectorStoreService()
+
+
 @router.post("/uploadfile/")
 async def create_upload_file(file: UploadFile, file_service: FileService = Depends(get_file_service),
                              pdf_service: PDFService = Depends(get_pdf_service),
                              chunk_service: ChunkService = Depends(get_chunks_service),
-                             embed_service: EmbeddingService = Depends(get_embeddings_service)):
+                             embed_service: EmbeddingService = Depends(get_embeddings_service),
+                             vector_store: VectorStoreService = Depends(get_vector_store_service)):
     file_path = await file_service.save_file(file)
 
     text = pdf_service.extract_text(file_path)
 
     chunks = chunk_service.split_text(text)
     embeddings_list = embed_service.embed_chunks(chunks)
+
+    # Add to Pinecone
+    vector_store.store_embeddings( embeddings_list, chunks, file.filename)
+    count = vector_store.count()
+    print(f"Total vectors in Pinecone: {count}")
 
     return {
         "message": "File processed",
---
+++ services/VectorStoreService.py
@@ -0,0 +1,56 @@
+import os
+from pinecone import Pinecone, ServerlessSpec
+
+
+class VectorStoreService:
+    def __init__(self):
+        api_key = os.getenv("PINECONE_API_KEY")
+        index_name = "resumes"
+
+        if not api_key:
+            raise ValueError("Missing PINECONE_API_KEY")
+
+        self.pc = Pinecone(api_key=api_key)
+
+        # Create index if not exists
+        existing_indexes = [idx.name for idx in self.pc.list_indexes()]
+
+        if index_name not in existing_indexes:
+            print("Creating Pinecone index...")
+
+            self.pc.create_index(
+                name=index_name,
+                dimension=768,  # must match Gemini embedding dimension
+                metric="cosine",
+                spec=ServerlessSpec(
+                    cloud="aws",
+                    region="us-east-1"   # choose same as your Pinecone project
+                )
+            )
+            print("Index created")
+
+        # Connect to index
+        self.index = self.pc.Index(index_name)
+
+    # Store embeddings
+    def store_embeddings(self, embeddings, chunks, filename):
+        vectors = []
+
+        for i, emb in enumerate(embeddings):
+            vectors.append({
+                "id": f"{filename}_{i}",
+                "values": emb,   # Gemini embedding values
+                "metadata": {
+                    "text": chunks[i],
+                    "filename": filename
+                }
+            })
+
+        self.index.upsert(vectors=vectors)
+
+        print(f"Stored {len(vectors)} vectors in Pinecone")
+
+    # Count vectors (debug)
+    def count(self):
+        stats = self.index.describe_index_stats()
+        return stats["total_vector_count"]
